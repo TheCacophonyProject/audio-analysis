6262b66d8473ce2e88f400d5b998dcaae43d3288
Chris.Blackbourn <CB@mac.com>
2019-11-03 17:48:54 +1300

    Lint.

--- Melt/melt.py
+++ Melt/melt.py
@@ -20,9 +20,11 @@ Melt
 
 
 def setup():
+    """Hints for setting up virtualenv and ffmpeg."""
     osn = common.get_os_short_name()
     venv_prefix = common.get_venv_prefix()
     dir_name = common.get_config_dir()
+
     if osn == 'lnx':
         # if symlink problem:
             # VBoxManage setextradata VM_NAME
@@ -30,17 +32,27 @@ def setup():
 
         print('sudo apt-get install ffmpeg')
         print('sudo apt-get install python3-venv')
+
+    if osn == 'mac':
+        print('brew install ffmpeg')
+
+    if osn == 'win':
+        print('Install https://www.ffmpeg.org/download.html')
+
     cmd = 'python3 -m venv %s' % dir_name
     print(cmd)
     cmd = venv_prefix + ' pip install numpy scipy'
     print(cmd)
-    ptf = 'tensorflow' if osn == 'mac' else 'tensorflow-gpu'
-    cmd = venv_prefix + ' pip install %s' % ptf
+    cmd = venv_prefix + ' pip install tensorflow-gpu'
+    print(cmd)
+    cmd = venv_prefix + ' pip install seaborn pandas matplotlib'
     print(cmd)
     print(venv_prefix)
 
 
 def main():
+    """Main entrypoint for Melt, in normal usage, everything starts here."""
+
     argv = sys.argv
     venv_prefix = common.get_venv_prefix()
     dir_name = common.get_config_dir()
@@ -54,9 +66,9 @@ def main():
         return 0
 
     suffix = argv[1].split('.')[-1].lower()
-    if suffix in '3gp,aac,ac3,adts,aif,aifc,caf,dts,dtshd,flac,gsm,m4a,mp3,mpa,oga,ogg,ra,rif,wav'.split(
-            ','):
-        cmd = '%s python chain.py -examine %s' % (venv_prefix, argv[1])
+    exts = '3gp,aac,ac3,adts,aif,aifc,caf,dts,dtshd,flac,gsm,m4a,mp3,mp4,mpa,oga,ogg,ra,rif,wav'
+    if suffix in exts.split(','):
+        cmd = '%s python chain.py -examine "%s"' % (venv_prefix, argv[1])
         common.execute(cmd)
         return 0
 
