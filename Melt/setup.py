5fcce1fead0b040eba10ae7aae2799d990ea06e7
gferraro <g.ferraro22@gmail.com>
2021-05-12 11:56:46 +1200

    add black

--- Melt/setup.py
+++ Melt/setup.py
@@ -1,27 +1,26 @@
 from distutils.core import setup
 
 scripts = [
-    'cacophony_index.py',
-    'chain.py',
-    'common.py',
-    'identify_species.py',
+    "cacophony_index.py",
+    "chain.py",
+    "common.py",
+    "identify_species.py",
 ]
 
 reqs = [
-    'librosa',
-    'numpy',
-    'scipy',
-    'tensorflow',
+    "librosa",
+    "numpy",
+    "scipy",
+    "tensorflow",
 ]
 
-data = [
-    ('bin/model', ['model/model_sd_aa.h5'])
-]
+data = [("bin/model", ["model/model_sd_aa.h5"])]
 
-setup(name='melt',
-      version='0.12',
-      description='A multitool for turning raw data, mostly audio, into structured information.',
-      scripts=scripts,
-      install_requires=reqs,
-      data_files=data,
-      )
+setup(
+    name="melt",
+    version="0.12",
+    description="A multitool for turning raw data, mostly audio, into structured information.",
+    scripts=scripts,
+    install_requires=reqs,
+    data_files=data,
+)
