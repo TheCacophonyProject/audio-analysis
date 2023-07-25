85a34ff4e5142d1853317e8c21f1e827d8027d82
gferraro <g.ferraro22@gmail.com>
2023-07-25 16:21:58 +1200

    update max chirps

--- Melt/identify_tracks.py
+++ Melt/identify_tracks.py
@@ -27,7 +27,7 @@ SIGNAL_WIDTH = 0.25
 # roughly the max possible chirps
 # assuming no more than 3 birds at any given moment
 def get_max_chirps(length):
-    return int(3 * (length / (SIGNAL_WIDTH + 0.01)))
+    return int(length / (SIGNAL_WIDTH + 0.01))
 
 
 def load_recording(file, resample=48000):
